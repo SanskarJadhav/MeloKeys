d34ebf3dbb75c6f15f9377ccc14a97ded98cc8dd
sshenoy03 <107995491+sshenoy03@users.noreply.github.com>
2023-04-08 22:19:28 +0530

    Add files via upload
    .form files are compiled outputs of .java files , change the path of the image as per the name and location for images to be displayed in bg in your system

---
+++ Frontend/Pijproject.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author sah-1
+ */
+public class Pijproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
